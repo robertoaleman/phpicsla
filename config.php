a3b481ca4d982fa3eaa7f737310369df2278a8c4
Roberto Aleman <ventics@gmail.com>
2025-03-14 12:51:53 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* PHP Image Comparison with Simple Learning Algorithm v1.02
-Author: Roberto Aleman
-ventics.com */
-$max_file_size = 2097152; // 2MB (en bytes)
-$allowed_types = ['image/jpeg', 'image/png', 'image/gif'];
-?>
